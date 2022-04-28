22465153a822fc47b46b24f20141e52f97dee7fd
Aum Bhanderi (Student) <abhander@sundevils.asu.edu>
2022-04-27 23:50:35 -0700

    initial commit for US2 - Save Selected Dots Implement Function

--- src/DBScan/ManageDots.java
+++ src/DBScan/ManageDots.java
@@ -8,7 +8,31 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ManageDots {
-
 	
+	public void saveSelectedDots(ArrayList<Integer[]> list) {
+		try {
+		      File myObj = new File("selectedDots.txt");
+		      if (myObj.createNewFile()) {
+		        System.out.println("File created: " + myObj.getName());
+		      } else {
+		        System.out.println("File already exists.");
+		      }
+		      
+		      FileWriter myWriter = new FileWriter("selectedDots.txt");
+		      
+		      for(int i = 0 ; i < list.size(); i++) {
+		    	  myWriter.write(list.get(i)[0] + "," + (list.get(i)[1] + "\n"));
+		      }
+		     
+		      myWriter.close();
+		      System.out.println("Successfully wrote to the file.");
+		      
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+	}
+
+
 	
 }
